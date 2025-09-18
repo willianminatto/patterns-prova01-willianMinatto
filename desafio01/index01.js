469ab83ad8f9909579c6b42c6801392e7ebde5ef
Willian Minatto <willian.279222@alunosatc.edu.br>
2025-09-18 19:34:01 -0300

    prova

--- desafio01/index01.js
+++ desafio01/index01.js
@@ -1,1 +1,76 @@
-console.log("Hello, World! 001");
+class Bicicleta {
+    move() {
+      return "Pedalando a bicicleta no pátio da UNISATC";
+    }
+  }
+  
+  class Patinete {
+    move() {
+      return "Andando de patinete pelo estacionamento da UNISATC";
+    }
+  }
+  
+  class Onibus {
+    move() {
+      return "Andando de ônibus e chegando na UNISATC";
+    }
+  }
+  
+  class TransporteFactory {
+    createIndividual() {
+      throw new Error("Método abstrato deve ser implementado");
+    }
+    createColetivo() {
+      throw new Error("Método abstrato deve ser implementado");
+    }
+  }
+  
+  class EcoFactory extends TransporteFactory {
+    createIndividual() {
+      return new Bicicleta();
+    }
+    createColetivo() {
+      return new Patinete();
+    }
+  }
+  
+  class PublicoFactory extends TransporteFactory {
+    createIndividual() {
+      return new Patinete();
+    }
+    createColetivo() {
+      return new Onibus();
+    }
+  }
+  
+  const factories = {
+    eco: new EcoFactory(),
+    publico: new PublicoFactory(),
+  };
+  
+  class TransporteApp {
+    constructor(factory) {
+      this.individual = factory.createIndividual();
+      this.coletivo = factory.createColetivo();
+    }
+  
+    iniciarTransporte() {
+      console.log(this.individual.move());
+      console.log(this.coletivo.move());
+    }
+  }
+  
+  function main() {
+    const tipos = ["eco", "publico"];
+  
+    tipos.forEach((tipo) => {
+      console.log(`\n>> Fábrica selecionada: ${tipo} <<`);
+      const factory = factories[tipo];
+      if (!factory) throw new Error("Tipo de fábrica desconhecido");
+  
+      const app = new TransporteApp(factory);
+      app.iniciarTransporte();
+    });
+  }
+  
+  main();
--- desafio01/index02.js
+++ desafio01/index02.js
@@ -1,1 +1,35 @@
-console.log("Hello, World! 002");
+class Config {
+    constructor() {
+      if (Config.instance) {
+        return Config.instance;
+      }
+      this.settings = {};
+      Config.instance = this;
+    }
+  
+    setConfig(key, value) {
+      this.settings[key] = value;
+      console.log(`Configuração definida: ${key} = ${value}`);
+    }
+  
+    getConfig(key) {
+      return this.settings[key];
+    }
+  
+    printAll() {
+      console.log("Configurações atuais:", this.settings);
+    }
+  }
+  
+  const c1 = new Config();
+  c1.setConfig("lang", "pt-BR");
+  
+  const c2 = new Config();
+  c2.setConfig("theme", "dark");
+  
+  const c3 = new Config();
+  console.log("Idioma atual:", c3.getConfig("lang"));
+  
+  c1.printAll();
+  c2.printAll();
+  c3.printAll();
